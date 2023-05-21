72f54d96f841cf80d49951aa471ce73521bb2fa2
Shaun <shaunshaju2014@gmail.com>
2023-05-21 20:50:36 +0530

    Commit #1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React,{ useState }from 'react';
 import './App.css';
 
+let dob =''
 function App() {
+  const [currentValue, setCurrentValue] = useState('')
+  // const [date, setDate] = useState('')
+  
+  
+  function storeDate(event){
+    event.preventDefault()
+    dob = currentValue
+    setCurrentValue('')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <form action="" method='GET' onSubmit={storeDate}>
+        <input type="date" value={currentValue} onChange={(event)=>{
+          setCurrentValue(event.target.value)
+        }}/>
+        <button type="submit">Submit</button>
+        <p>{dob}</p>
+      </form>
+    </main>
   );
 }
 
